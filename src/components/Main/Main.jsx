7a38351df3fc98cb7acffbb8897bf1c59e815f05
JosejCruz <jcruz@medicaresoftsrl.com>
2022-08-23 14:39:32 -0400

    Tercer commit
    arreglando algunos errores

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -6,32 +6,38 @@ import './Main.css'
 
 function main() {
   return (
-    <div className="container">
+    <div className="container-fluid">
       <div className="row">
+        <div className='row'>
         <div className="col-sm-12 col-md-4 col-lg-4 pt-4">
           <Select/>
         </div>
-        <div className='col-sm-12 col-md-8 col-lg-8'></div>
+        </div>
+        <div className='row'>
         <div className="col-sm-12 col-md-4 col-lg-4">
           <h3 className='text-center'>Cantidad de Estudios:</h3>
         </div>
-        <div className='col-sm-12 col-md-8 col-lg-8'></div>
+        </div>
+        <div className='row'>
         <Items/>
-        <div className='col-sm-12 col-md-8 col-lg-8'></div>
+        </div>
+        <div className='row'>
         <ItemsTotal/>
-        <div className='col-sm-12 col-md-8 col-lg-8'></div>
-        <div className="d-grid gap-2 mx-auto text-center col-sm-12 col-md-4 col-lg-4 pt-4">
+        </div>
+        <div className='row'>
+        <div className="d-grid gap-2 text-center col-sm-12 col-md-4 col-lg-4 pt-4">
           <button className="color-button">
           <ion-icon name="search-outline"></ion-icon> Buscar
           </button>
         </div>
-        <div className='col-sm-12 col-md-8 col-lg-8'></div>
-        <div className="col-sm-12 col-md-4 col-lg-4 pt-4 d-grid gap-2 mx-auto text-center ">
+        </div>
+        <div className='row'>
+        <div className="col-sm-12 col-md-4 col-lg-4 pt-4 d-grid gap-2 text-center ">
           <button className="color-button">
           <ion-icon name="cloud-download-outline"></ion-icon> Descargar
           </button>
         </div>
-        <div className='col-sm-12 col-md-8 col-lg-8'></div>
+        </div>
       </div>
     </div>
   );
