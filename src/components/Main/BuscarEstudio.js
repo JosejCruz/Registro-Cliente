1304425875b9837afd6aede286160db54f01436c
JosejCruz <jcruz@medicaresoftsrl.com>
2022-08-24 17:36:14 -0400

    Peticion con Axios

---
+++ src/components/Main/BuscarEstudio.js
@@ -0,0 +1,40 @@
+import axios from 'axios'
+import moment from 'moment'
+
+
+
+// {
+//     "ID": 178290,
+//     "FECHA": "20220824",
+//     "NOMBRE": "ESPINOZA^VISCARRA^DANILO",
+//     "IUD": "14948",
+//     "SEXO": "M",
+//     "PAS_ID": "014149",
+//     "EST_UID": "78cc8d63-a6188294-6ab076d3-ee77f343-af319a92",
+//     "SERIES": [
+//         {
+//             "SER_UID": 178291,
+//             "SER_ID": "71b5068b-3d277901-35a423f5-9a9b6ee8-53c1795e",
+//             "MODALIDAD": "DX", // contar cuantos hay de cada clase //
+//             "NOMBRE": "AP",
+//             "FILES": [
+//                 {
+//                     "POS": "1",
+//                     "INS_UID": "e51b556a-9e36-49b7-bb1e-3fe554a3c0cf",
+//                     "AE": "MEDICALTECGW"
+//                 }
+//             ]
+//         }
+//     ]
+// }
+const BuscarEstudios = async (Cliente, Fechas) =>{
+    console.log('Buscando')
+    console.log(Cliente)
+    console.log(Fechas)
+    const fi = moment(Fechas.inicial, "YYYY-MM-DD").format("YYYYMMDD")
+    const ff = moment(Fechas.final, "YYYY-MM-DD").format("YYYYMMDD")
+    console.log(fi, ff)
+    let datos = await axios.get(`${Cliente.url}/getestudios?inicio=${fi}&${ff}`)
+    console.log(datos.data)
+}  
+export default BuscarEstudios
